976d352f0257efe043215e2d80c91874a86aa45e
BenjaminLM95 <benjamin.lerner95@gmail.com>
2024-11-13 11:02:13 -0400

    Camera adjustments

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,13 +6,15 @@ public class CameraController : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject player;
-    private Vector3 offset;
-    //public float horizontalSpeed = 0.5f;
+    private Vector3 offset = new Vector3(0.0f, 0.0f, 0.0f);
+    public float horizontalSpeed = 0.5f;
+    float cameraFieldView;
+     
 
     void Start()
     {
-        
-        offset = transform.position - player.transform.position; 
+        cameraFieldView = Camera.main.fieldOfView; 
+        offset = transform.position - player.transform.position;        
     }
 
     // Update is called once per frame
@@ -23,8 +25,26 @@ public class CameraController : MonoBehaviour
 
     private void Update()
     {
-        //float h = horizontalSpeed * Input.GetAxis("Mouse X");
-        //transform.eulerAngles += new Vector3(0, h, 0); 
+        float h = horizontalSpeed * Input.GetAxis("Mouse X");
+        transform.eulerAngles += new Vector3(0, h, 0); 
+
+
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            if(Camera.main.fieldOfView < 80.0f)
+            Camera.main.fieldOfView += 1.0f; 
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            if (Camera.main.fieldOfView > 40.0f)
+                Camera.main.fieldOfView -= 1.0f;
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Camera.main.fieldOfView = cameraFieldView;
+        }
+
+
     }
 
 }
